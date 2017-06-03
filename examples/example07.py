38a7000b82fc15de790f825c9d26a9b49c12c261
Arend van Spriel <aspriel@gmail.com>
2017-06-03 22:29:37 +0200

    examples: cleanup scheduled scan example
    Signed-off-by: Arend van Spriel <aspriel@gmail.com>

--- examples/example07.py
+++ examples/example07.py
@@ -3,12 +3,13 @@ import netlink.capi as nl
 import sys
 
 ifidx = nl.if_nametoindex(sys.argv[1])
-print("%s: ifidx=%d" % (sys.argv[1], ifidx))
+level = nl.NL_CB_DEBUG
 if sys.argv[2] == 'start':
-	rh = sched_scan_start(ifidx)
+	rh = sched_scan_start(ifidx, level)
 	rh.add_matches([{ 'ssid': 'lemonhead'}])
 	rh.set_interval(30000)
 elif sys.argv[2] == 'stop':
-	rh = sched_scan_stop(ifidx, nl.NL_CB_DEBUG)
+	rh = sched_scan_stop(ifidx, level)
 
-rh.send()
+err = rh.send()
+print("scheduled scan %s: result=%d" % (sys.argv[2], err))
